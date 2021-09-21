6a38570dcce81dff8527152b1895eea50702d0b4
Alex Maior <alexmaior@yahoo.com>
2021-09-21 21:18:34 +0300

    Added a button for the dark mode and added some functionality, still needs work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./App.module.css";
 import Button from "./components/Button";
 import { ImArrowRight } from "react-icons/im";
 import { IoSunnyOutline } from "react-icons/io5";
 import { ParticlesComponent } from "./components/ParticlesComponent";
 
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "./components/theme";
+import { GlobalStyles } from "./components/global";
+
 function App() {
+  const [theme, setTheme] = useState("light");
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
   return (
-    <div className={classes.App}>
-      <div className={classes.titleDescription}>
-        <p>Hello, I'm </p>
-        <span>Alex Maior</span>
-        <br />
-        <p>a full-stack web developer</p>
-        <br />
-        <Button className={classes.btn}>
-          VIEW MY WORK <ImArrowRight className={classes.arrow} />
-        </Button>
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <div className={classes.App}>
+        <GlobalStyles />
+        <div className={classes.titleDescription}>
+          <Button onClick={toggleTheme} className={classes.btn}>
+            <IoSunnyOutline />
+          </Button>
+          <p>Hello, I'm </p>
+          <span>Alex Maior</span>
+          <br />
+          <p>a full-stack web developer</p>
+          <br />
+          <Button className={classes.btn}>
+            VIEW MY WORK <ImArrowRight className={classes.arrow} />
+          </Button>
+        </div>
+        <ParticlesComponent />
       </div>
-      <ParticlesComponent />
-    </div>
+    </ThemeProvider>
   );
 }
 
