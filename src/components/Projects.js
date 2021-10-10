9f70af858792c1a7371b0ea65a8e9d3bc9b780f8
Alex Maior <alexmaior@yahoo.com>
2021-10-10 22:05:39 +0300

    small change

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,24 +9,24 @@ const Projects = () => {
       <h2>Projects</h2>
       <article className={classes.projectsImagesContainer}>
         <div>
-          <a href="#" target="_blank" rel="noreferrer">
-            <img
-              src={SibiuGuide}
-              alt="Sibiu City Guide"
-              className={classes.projectImage}
-            />
-            <p>Sibiu City Guide</p>
-          </a>
+          {/* <a href="#" target="_blank" rel="noreferrer"> */}
+          <img
+            src={SibiuGuide}
+            alt="Sibiu City Guide"
+            className={classes.projectImage}
+          />
+          <p>Sibiu City Guide</p>
+          {/* </a> */}
         </div>
         <div>
-          <a href="#" target="_blank" rel="noreferrer">
-            <img
-              src={Sushi}
-              alt="Sushi Restaurant"
-              className={classes.projectImage}
-            />
-            <p>Sushi Restaurant</p>
-          </a>
+          {/* <a href="#" target="_blank" rel="noreferrer"> */}
+          <img
+            src={Sushi}
+            alt="Sushi Restaurant"
+            className={classes.projectImage}
+          />
+          <p>Sushi Restaurant</p>
+          {/* </a> */}
         </div>
         <div>
           <a
