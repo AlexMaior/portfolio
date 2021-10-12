adeb3ef2e17335dfac0ce8ebf9485571b42c4834
Alex Maior <alexmaior@yahoo.com>
2021-10-12 14:50:04 +0300

    Added the links to the project images

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,24 +9,32 @@ const Projects = () => {
       <h2>Projects</h2>
       <article className={classes.projectsImagesContainer}>
         <div>
-          {/* <a href="#" target="_blank" rel="noreferrer"> */}
-          <img
-            src={SibiuGuide}
-            alt="Sibiu City Guide"
-            className={classes.projectImage}
-          />
-          <p>Sibiu City Guide</p>
-          {/* </a> */}
+          <a
+            href="https://sibiucityguide.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src={SibiuGuide}
+              alt="Sibiu City Guide"
+              className={classes.projectImage}
+            />
+            <p>Sibiu City Guide</p>
+          </a>
         </div>
         <div>
-          {/* <a href="#" target="_blank" rel="noreferrer"> */}
-          <img
-            src={Sushi}
-            alt="Sushi Restaurant"
-            className={classes.projectImage}
-          />
-          <p>Sushi Restaurant</p>
-          {/* </a> */}
+          <a
+            href="https://sushi-website.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src={Sushi}
+              alt="Sushi Restaurant"
+              className={classes.projectImage}
+            />
+            <p>Sushi Restaurant</p>
+          </a>
         </div>
         <div>
           <a
